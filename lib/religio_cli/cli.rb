cdf87c044418462e0dc192f82a40f1792e5e9051
Lois Lee <lois.lee@me.com>
2019-02-03 19:07:44 +0000

    added interface language to cli.rb

--- lib/religio_cli/cli.rb
+++ lib/religio_cli/cli.rb
@@ -1,9 +1,29 @@
 class ReligioCLI::CLI
-  def call
 
+  def call
+    puts "Hello there! Welcome to Religio. I offer information on the top 50 Major Religious traditions. Please see the list below, presented in alphabetical order."
+    puts " "
+    display_list
+    menu
+    goodbye
   end
 
   def list_religions
 
   end
+
+  def display_religion(integer)
+
+  end
+
+  def menu
+    puts "For details on a particular tradition, type the number associated."
+    puts "To exit at any time, enter 'exit'."
+    puts "Which religious tradition would you like to learn more about?"
+  end
+
+  def goodbye
+    puts "Thanks for exploring! We hope you learned something cool today. Feel free to visit anytime : )"
+  end
+
 end
