7de24486919c67c2a4cd74cb27abd3a063d3d4af
Lois Lee <lois.lee@me.com>
2019-02-03 18:40:14 +0000

    added new files: cli.rb, trads.rb, scraper.rb; modified religio_cli.rb

--- lib/religio_cli.rb
+++ lib/religio_cli.rb
@@ -1,4 +1,13 @@
+require "open-uri"
+require "pry"
+require "nokogiri"
+
 require "religio_cli/version"
+require "religio_cli/cli"
+require "religio_cli/scraper"
+require "religio_cli/trads"
+
+
 
 module ReligioCli
   class Error < StandardError; end
