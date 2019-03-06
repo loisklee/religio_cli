2800e0f763b1f7f1d5efa2790a2a82d960ae2247
Lois Lee <lois.lee@me.com>
2019-03-06 20:39:16 +0000

    updated CLI logic, changed call

--- lib/religio_cli/cli.rb
+++ lib/religio_cli/cli.rb
@@ -2,10 +2,20 @@ class ReligioCLI::CLI
 
   def call
     ReligioCLI::Scraper.initiate_scraper
-    puts "Hello there! Welcome to Religio. I offer information on the top 50 Major Religious traditions. Please see the list below, presented in alphabetical order."
-    puts " "
-    list_religions
-    menu
+    puts "Hello there! Welcome to Religio. I offer information on the top 50 Major Religious traditions. To see the list presented in alphabetical order, enter 'list'. To exit, enter 'exit'."
+    input = nil
+    input = gets.strip.downcase
+    if input == "list"
+        list_religions
+        menu
+    elsif input == "exit"
+      goodbye
+      exit
+    else
+      puts "I'm not sure what you'd like. Please see the below the list:"
+      list_religions
+      menu
+    end
   end
 
   def list_religions
@@ -39,6 +49,7 @@ class ReligioCLI::CLI
       puts "Enter Y or N"
       input = gets.strip.downcase
       if input == "y"
+        list_religions
         menu
       elsif input == "n"
         goodbye
@@ -48,12 +59,14 @@ class ReligioCLI::CLI
         exit
       else
         puts "I'm not sure what you'd like. Please see the below the list:"
+        list_religions
         menu
       end
     elsif choice == "exit".downcase
       goodbye
     else
       puts "I'm not sure what you'd like. Please enter a number between 1 and 50. If you want to leave, enter 'exit'."
+      list_religions
       menu
     end
   end
